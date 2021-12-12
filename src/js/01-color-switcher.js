7afb3893d0f71c45a47bc74021ffe3718793a47a
Malanchenko Sergii <malan.sergii@gmail.com>
2021-12-12 21:04:53 +0200

    1-st done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+let timerId;
+const TIME_DELAY = 1000;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onStartButtonClick() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, TIME_DELAY);
+    buttonStart.disabled = true;
+};
+
+function onStopButtonClick() {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+};
+
+buttonStart.addEventListener('click', onStartButtonClick);
+buttonStop.addEventListener('click', onStopButtonClick );
